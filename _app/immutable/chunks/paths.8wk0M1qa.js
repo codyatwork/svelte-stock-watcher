92e0c3f538dc8cb5d63ddcd0c9d43cf8bb530d55
codyatwork <mims@hey.com>
2024-08-20 15:41:12 +0000

    Deploying to gh-pages from @ codyatwork/svelte-stock-watcher@f821af8ae390eaf95463b6a1c53ad4e245159f3e 🚀

---
+++ _app/immutable/chunks/paths.8wk0M1qa.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_ziss7u)==null?void 0:s.base)??"/svelte-stock-watcher";var e;const a=((e=globalThis.__sveltekit_ziss7u)==null?void 0:e.assets)??t;export{a,t as b};

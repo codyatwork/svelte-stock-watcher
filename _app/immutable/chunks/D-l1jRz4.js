6f280daca23c569a73f478ea792726755d1ea8ee
codyatwork <mims@hey.com>
2025-03-15 17:49:59 +0000

    Deploying to gh-pages from @ codyatwork/svelte-stock-watcher@dea30d05b2c3a0f03934e62114cc6501da9e2eba 🚀

---
+++ _app/immutable/chunks/D-l1jRz4.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1l9b4sw)==null?void 0:s.base)??"/svelte-stock-watcher";var e;const a=((e=globalThis.__sveltekit_1l9b4sw)==null?void 0:e.assets)??t;export{a,t as b};
